5fbd885d8009b1bef91cfcaa7fe8a281bc31cb9b
ingann <inka.niinisalo@helsinki.fi>
2024-02-12 22:31:24 +0200

    ex 1.8 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,20 +19,51 @@ const Statistics = (props) => {
   )
 }
 
+const Average = (props) => {
+  let result = 0
+  if (props.all != 0) {
+    result = props.value/props.all
+  }
+  return (
+    <div>
+      {props.text} {result}
+    </div>
+  )
+}
+
+const Positive = (props) => {
+  let result = 0
+  if (props.all != 0) {
+    result = (props.value/props.all) * 100
+  }
+  return (
+    <div>
+      {props.text} {result} %
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const handleGoodClick = () => {
+    setAll(allClicks + 1)
+    setAverage(average + 1)
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
+    setAll(allClicks + 1)
     setNeutral(neutral + 1)
   }
 
   const handleBadClick = () => {
+    setAll(allClicks + 1)
+    setAverage(average - 1)
     setBad(bad + 1)
   }
 
@@ -46,6 +77,9 @@ const App = () => {
       <Statistics text='good' value={good} />
       <Statistics text='neutral' value={neutral} />
       <Statistics text='bad' value={bad} />
+      <Statistics text='all' value={allClicks} />
+      <Average text='average' value={average} all={allClicks} />
+      <Positive text='positive' value={good} all={allClicks}/>
     </div>
   )
 }
