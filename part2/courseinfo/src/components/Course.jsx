b464777ab15a5b278ebb3a1fb77d9e3af3c94795
ingann <inka.niinisalo@helsinki.fi>
2024-02-14 14:34:46 +0200

    ex 2.2 and 2.3. courseinfo

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,18 +1,22 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Part = (props) => {
+const Part = ({ part, exercises}) => {
   return (
-      <p>{props.part} {props.exercises}</p>
+      <p>{part} {exercises}</p>
   )
 }
 
 const Course = ({ course }) => {
+    const total = course.parts.reduce(function(sum, part) {
+        return sum + part.exercises
+    }, 0)
   return (
     <div>
-      <Header course={course.name} />
+        <Header course={course.name} />
         {course.parts.map(part =>
           <Part key={part.id} part={part.name} exercises={part.exercises}/>
-          )}
+        )}
+        <div><strong>total of {total} exercises</strong></div>
     </div>
   )
 }
