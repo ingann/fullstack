9638d417db895204de4f1883909a1e41dd9aeb54
ingann <inka.niinisalo@helsinki.fi>
2024-03-06 14:48:00 +0200

    added weather component and api calls

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 
-const Weather = ( {capital} ) => {
+const Weather = ({ capital }) => {
     const api_key = import.meta.env.VITE_SOME_KEY
-    const [weather, setWeather] = useState([])
+    const [weather, setWeather] = useState(null)
 
     useEffect(() => {
-        axios
-            .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}}`)
-            .then(response => {
-                setWeather(response.data)
-            })
-    }, [])
+        const fetchWeatherData = async () => {
+            try {
+                const response = await axios
+                    .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${api_key}`)
+                    .then(response => {
+                        setWeather(response.data)
+                    })
+            } catch (error) {
+                console.error("Error fetching weather data:", error)
+            }
+        }
+        fetchWeatherData()
+    }, [capital, api_key])
+
+    if (!weather) {
+        return (
+            <div>Waiting for weather data</div>
+        )
+    }
+
     return (
         <div>
             <h2>Weather in {capital}</h2>
+            <p>temperature {weather.main.temp} Celcius</p>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={`${weather.weather[0].description} icon`}/>
+            <p>wind {weather.wind.speed} m/s</p>
         </div>
     )
 }
