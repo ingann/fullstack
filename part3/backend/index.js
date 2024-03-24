c31b095d3b00fc19ebe4e971bb89485b35c00086
ingann <inka.niinisalo@helsinki.fi>
2024-03-24 14:47:02 +0200

    3.15-3.18 corrected one typo

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -48,28 +48,58 @@ app.get('/info', (request, response) => {
     response.send(
       `<p>Phonebook has info for ${people.length} people</p>
       <p>${new Date()}</p>`
-  )
+    )
   })
-    
 })
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person=> {
-      response.json(person)
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
-app.delete('/api/persons/:id', (request, response) => {
-const id = Number(request.params.id)
-persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
