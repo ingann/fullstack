d7d3b5385701c50018a98e9e58ab95279d733e04
ingann <inka.niinisalo@helsinki.fi>
2024-03-26 17:32:51 +0200

    4.3 and 4.4 bloglist testing

---
+++ part4/blogs/utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = (blogs) => {
+    return 1
+  }
+
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+  module.exports = {
+    dummy,
+    totalLikes
+  }
+
