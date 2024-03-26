3d04462b157cf1bd1ff4288dfcb2579abc2bca02
ingann <inka.niinisalo@helsinki.fi>
2024-03-26 18:39:50 +0200

    ex 4.5 bloglist

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -8,8 +8,21 @@ const totalLikes = (blogs) => {
         : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0)
+        return 0
+    const favorite = blogs.reduce((fav, current) => fav.likes > current.likes ? fav : current)
+
+    return {
+        title: favorite.title, 
+        author: favorite.author, 
+        likes: favorite.likes
+    }
+}
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
 
