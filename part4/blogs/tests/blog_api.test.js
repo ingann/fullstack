936aa5dd5ec8a888975504b22393a7f9f6679281
ingann <inka.niinisalo@helsinki.fi>
2024-03-30 13:06:01 +0200

    ex 4.12 bloglist

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -89,6 +89,38 @@ test('there are two blogs', async () => {
         assert.strictEqual(createdBlog.likes, 0)
   })
 
+  test('blog without url is not added', async () => {
+    const newBlog = {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        likes: 0
+      }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
+
+  test('blog without title is not added', async () => {
+    const newBlog = {
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+      }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
+
 after(async () => {
   await mongoose.connection.close()
 })
