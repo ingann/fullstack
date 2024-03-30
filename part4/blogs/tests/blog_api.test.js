ad21e3817ed7bec611f2ece81f0ecdb55aa9b90e
ingann <inka.niinisalo@helsinki.fi>
2024-03-30 11:33:06 +0200

    ex 4.8-4.9 bloglist

---
+++ part4/blogs/tests/blog_api.test.js
@@ -0,0 +1,51 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const assert = require('node:assert')
+const helper = require('./test_helper')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    console.log(response.body)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+  
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('blog identifier is named as id instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    const validIds = blogs.map(blog => {
+        assert.ok(blog.id)
+        assert.strictEqual(blog._id, undefined)
+    })
+  })
+  
+after(async () => {
+  await mongoose.connection.close()
+})
